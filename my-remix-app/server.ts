00ed6d9c47d13603762498247e5fe4f5475b65b4
watany <76135106+watany-dev@users.noreply.github.com>
2023-08-30 07:00:28 +0000

    adding middleware

--- my-remix-app/server.ts
+++ my-remix-app/server.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import { env } from 'hono/adapter'
+import { basicAuth } from 'hono/basic-auth'
+import { secureHeaders } from 'hono/secure-headers'
 import type { AppLoadContext } from '@remix-run/cloudflare'
 import { createRequestHandler } from '@remix-run/cloudflare'
 import * as build from './build'
@@ -8,6 +10,15 @@ import * as build from './build'
 const handleRemixRequest = createRequestHandler(build, process.env.NODE_ENV)
 
 const app = new Hono()
+
+app.use(
+  '*',
+  basicAuth({
+    username: 'hono',
+    password: 'acoolproject',
+  })
+)
+app.use('*', secureHeaders())
 
 app.get('*', async (c) => {
   const loadContext: AppLoadContext = { env: env(c) }
